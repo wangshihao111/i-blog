87cc9d5945295878113f05e5ec4d88685cc9c280
shihao.wang <shihao.wang@hand-china.com>
2020-09-17 20:21:27 +0800

    feat: 修改标题

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
         id: 'post', // Unique id for current classifier
         dirname: '_posts', // Matched directory name
         path: '/', // Entry page for current classifier
-        title: '随笔', // Entry and pagination page titles for current classifier.
+        title: 'i博客', // Entry and pagination page titles for current classifier.
         // layout: 'IndexWriting', // Layout component name for entry page.
         frontmatter:{ //Front matter for entry page.
           tag: 'vuepress'
